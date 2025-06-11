a881c4a60a739de559a8efdef04bc4e02a0f2af7
Farhan0021 <mdfarhanahmed2100@gmail.com>
2025-06-11 16:05:16 +0530

    ok

--- dist/assets/js/custom.js
+++ dist/assets/js/custom.js
@@ -15,14 +15,14 @@
 		var overlay = document.getElementById('overlayer');
 
 		function fadeOut(el) {
-			// el.style.opacity = 1;
-			// (function fade() {
-			// 	if ((el.style.opacity -= .1) < 0) {
-			// 		el.style.display = "none";
-			// 	} else {
-			// 		requestAnimationFrame(fade);
-			// 	}
-			// })();
+			el.style.opacity = 1;
+			(function fade() {
+				if ((el.style.opacity -= .1) < 0) {
+					el.style.display = "none";
+				} else {
+					requestAnimationFrame(fade);
+				}
+			})();
 		};
 
 		setTimeout(function() {
