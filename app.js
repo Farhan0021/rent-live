e25fc655b8a5368098a2df4fd006d4b57930d3d4
Farhan0021 <mdfarhanahmed2100@gmail.com>
2025-06-11 19:35:00 +0530

    ok2

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ import allRoutes from './routes/allRoutes.js';
 import cors from 'cors'
 import upload from 'express-fileupload'
 import path from 'path';
-
-
-
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }));
@@ -13,18 +10,7 @@ app.use(express.static(path.resolve()+"/assets"));
 app.use(cors());
 app.use(upload());
 
-/* thi for deployment ---------start---------*/
-const root = path.join(path.resolve(), "dist");
-app.use(express.static(root));
-/* tis depluy -------end----------*/
-
 app.use(allRoutes);
-
-/* this deplu -----start-------*/
-app.get("*", (req,res)=>{
-    res.sendFile("index.html", {root});
-})
-
 const port = process.env.PORT;
 app.listen(port, ()=>{
     console.log("Server Running with port ", port);
